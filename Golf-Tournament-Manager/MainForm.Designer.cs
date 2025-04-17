75d1a9b375ee151f6ee49c0a61647eda53b43905
braeden-miller <braeden.miller@trojans.dsu.edu>
2025-04-17 16:28:44 -0500

    Created Project, Tested some controls

---
+++ Golf-Tournament-Manager/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Golf_Tournament_Manager
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.tblLeaderboard = new System.Windows.Forms.DataGridView();
+            this.PlayerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NetScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tblLeaderboard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tblLeaderboard
+            // 
+            this.tblLeaderboard.AllowUserToAddRows = false;
+            this.tblLeaderboard.AllowUserToDeleteRows = false;
+            this.tblLeaderboard.AllowUserToResizeColumns = false;
+            this.tblLeaderboard.AllowUserToResizeRows = false;
+            this.tblLeaderboard.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tblLeaderboard.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.tblLeaderboard.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblLeaderboard.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PlayerName,
+            this.NetScore});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.tblLeaderboard.DefaultCellStyle = dataGridViewCellStyle3;
+            this.tblLeaderboard.Location = new System.Drawing.Point(1084, 12);
+            this.tblLeaderboard.Name = "tblLeaderboard";
+            this.tblLeaderboard.ReadOnly = true;
+            this.tblLeaderboard.RowHeadersVisible = false;
+            this.tblLeaderboard.RowHeadersWidth = 51;
+            this.tblLeaderboard.RowTemplate.Height = 24;
+            this.tblLeaderboard.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tblLeaderboard.Size = new System.Drawing.Size(303, 622);
+            this.tblLeaderboard.TabIndex = 0;
+            // 
+            // PlayerName
+            // 
+            this.PlayerName.FillWeight = 200F;
+            this.PlayerName.HeaderText = "Player";
+            this.PlayerName.MinimumWidth = 6;
+            this.PlayerName.Name = "PlayerName";
+            this.PlayerName.ReadOnly = true;
+            // 
+            // NetScore
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.NetScore.DefaultCellStyle = dataGridViewCellStyle2;
+            this.NetScore.HeaderText = "Net";
+            this.NetScore.MinimumWidth = 6;
+            this.NetScore.Name = "NetScore";
+            this.NetScore.ReadOnly = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1399, 913);
+            this.Controls.Add(this.tblLeaderboard);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Golf Tournament Manager";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblLeaderboard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tblLeaderboard;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PlayerName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NetScore;
+    }
+}
+
---
+++ Golf-Tournament-Manager/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Golf_Tournament_Manager
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            tblLeaderboard.Sort(tblLeaderboard.Columns["NetScore"], ListSortDirection.Descending);
+            Random random = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                tblLeaderboard.Rows.Add("Test", random.Next(-5,15));
+            }
+        }
+    }
+}
